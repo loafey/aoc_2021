56170ea3d5999a6d8159e97fef4cb949c1d93038
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2021-12-02 08:19:05 +0100

    🏌️

--- src/day2/mod.rs
+++ src/day2/mod.rs
@@ -1,16 +1,16 @@
 use aoc_2021::load_to_pattern;
 
+// 0.0024
 pub fn part1() -> i32 {
     let mut hoz = 0;
     let mut dep = 0;
     let mut direction = String::new();
     load_to_pattern("input/day2.txt", |c| c == ' ').for_each(|w| match w.parse::<i32>() {
-        Ok(v) => match &direction[..] {
-            "forward" => hoz += v,
-            "down" => dep += v,
-            "up" => dep -= v,
-            _ => {}
-        },
+        Ok(v) => {
+            hoz += (&direction[..] == "forward") as i32 * v;
+            dep += (&direction[..] == "down") as i32 * v;
+            dep -= (&direction[..] == "up") as i32 * v;
+        }
         Err(_) => direction = w,
     });
 
@@ -23,15 +23,12 @@ pub fn part2() -> i32 {
     let mut aim = 0;
     let mut direction = String::new();
     load_to_pattern("input/day2.txt", |c| c == ' ').for_each(|w| match w.parse::<i32>() {
-        Ok(v) => match &direction[..] {
-            "forward" => {
-                hoz += v;
-                dep += v * aim;
-            }
-            "down" => aim += v,
-            "up" => aim -= v,
-            _ => {}
-        },
+        Ok(v) => {
+            hoz += (&direction[..] == "forward") as i32 * v;
+            dep += (&direction[..] == "forward") as i32 * v * aim;
+            aim += (&direction[..] == "down") as i32 * v;
+            aim -= (&direction[..] == "up") as i32 * v;
+        }
         Err(_) => direction = w,
     });
 
