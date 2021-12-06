59034905b39ad2df59b69caf5163fab3528ee091
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2021-12-06 12:44:10 +0100

    Optimized day 5 a bit.

--- src/day5/mod.rs
+++ src/day5/mod.rs
@@ -28,6 +28,21 @@ impl Pos {
     }
 }
 
+fn delta(p1: &Pos, p2: &Pos) -> (i32, i32) {
+    (
+        match p1.x.cmp(&p2.x) {
+            Ordering::Less => 1,
+            Ordering::Equal => 0,
+            Ordering::Greater => -1,
+        },
+        match p1.y.cmp(&p2.y) {
+            Ordering::Less => 1,
+            Ordering::Equal => 0,
+            Ordering::Greater => -1,
+        },
+    )
+}
+
 fn solver<A>(path: A, filter: fn(&(Pos, Pos)) -> bool) -> usize
 where
     A: AsRef<Path> + std::fmt::Debug + Copy,
@@ -49,31 +64,24 @@ where
             .reduce(|a, v| if a >= v { a } else { v })
             .unwrap_or_default()
     };
-    let delta = |p1: i32, p2: i32| match p1.cmp(&p2) {
-        Ordering::Less => 1,
-        Ordering::Equal => 0,
-        Ordering::Greater => -1,
-    };
 
     let largest_x = largest(|(p1, p2)| [p1.x, p2.x]);
     let largest_y = largest(|(p1, p2)| [p1.y, p2.y]);
 
-    let mut matrix = vec![vec![0; largest_x as usize + 1]; largest_y as usize + 1];
+    let mut matrix = vec![0; ((largest_x + 1) * (largest_y + 1)) as usize];
 
     s.iter().for_each(|(p1, p2)| {
-        let x_mod = delta(p1.x, p2.x);
-        let y_mod = delta(p1.y, p2.y);
+        let (x_mod, y_mod) = delta(p1, p2);
 
-        let mut x = p1.x;
-        let mut y = p1.y;
+        let (mut x, mut y) = (p1.x, p1.y);
         while x != p2.x + x_mod || y != p2.y + y_mod {
-            matrix[y as usize][x as usize] += 1;
+            matrix[(y + (x * largest_y)) as usize] += 1;
             x += x_mod;
             y += y_mod;
         }
     });
 
-    matrix.iter().flatten().filter(|v| **v > 1).count()
+    matrix.into_iter().filter(|v| *v > 1).count()
 }
 
 pub fn part1() -> usize {
