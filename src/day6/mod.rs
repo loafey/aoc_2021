a4e5b3f68f1bf68ab0fcb28040750c9c24d54757
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2021-12-06 10:16:31 +0100

    🦦 Day 6 is fast af ⚡

--- src/day6/mod.rs
+++ src/day6/mod.rs
@@ -1,7 +1,28 @@
-pub fn part1() -> String {
-    String::new()
+use aoc_lib::load_to_pattern;
+
+pub fn part1() -> usize {
+    solver(80)
 }
 
-pub fn part2() -> String {
-    String::new()
+pub fn part2() -> usize {
+    solver(256)
+}
+
+fn solver(end_day: usize) -> usize {
+    let mut fishies = [0i128; 10];
+    load_to_pattern("input/day6.txt", |c| c == ',')
+        .for_each(|v| fishies[v.parse::<usize>().unwrap()] += 1);
+    for _ in 0..end_day {
+        for i in 0..fishies.len() {
+            if i == 0 {
+                fishies[9] += fishies[i];
+                fishies[7] += fishies[i];
+                fishies[0] = 0;
+            } else {
+                fishies[i - 1] += fishies[i];
+                fishies[i] = 0;
+            }
+        }
+    }
+    fishies.iter().map(|f| *f as usize).sum()
 }
