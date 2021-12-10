356940b1d067f7fb18d0583fb5376eb40f192276
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2021-12-10 08:57:31 +0100

    This one was not too hard, very funky solve.

--- src/day10/mod.rs
+++ src/day10/mod.rs
@@ -1,7 +1,211 @@
-pub fn part1() -> String {
-    String::new()
+use aoc_lib::load_to_matrix;
+use interior_mutability_pointer::Imp;
+/*
+       let row_str = r.clone().collect::<String>();
+       let mut square = 0;
+       let mut curly = 0;
+       let mut paran = 0;
+       let mut arrow = 0;
+       r.for_each(|c| match c {
+           '(' => paran += 1,
+           ')' => paran -= 1,
+           '[' => square += 1,
+           ']' => square -= 1,
+           '{' => curly += 1,
+           '}' => curly -= 1,
+           '<' => arrow += 1,
+           '>' => arrow -= 1,
+           _ => {}
+       });
+
+       if square != 0 && curly != 0 && paran != 0 && arrow != 0 {
+           println!("{}", row_str)
+       }
+*/
+
+#[derive(Debug, Clone)]
+struct Chunk {
+    char: char,
+    children: Vec<Imp<Chunk>>,
+    parent: Option<Imp<Chunk>>,
 }
 
-pub fn part2() -> String {
-    String::new()
+impl std::fmt::Display for Chunk {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
+        write!(f, "{}", self.d(true))
+    }
+}
+
+impl Chunk {
+    fn d(&self, first: bool) -> String {
+        let end = match self.char {
+            '(' => ')',
+            '{' => '}',
+            '[' => ']',
+            '<' => '>',
+            _ => '-',
+        };
+        let middle = {
+            let mut s = String::new();
+            self.children
+                .iter()
+                .for_each(|child| s = format!("{}{}", s, child.d(false)));
+            s
+        };
+        if first {
+            middle
+        } else {
+            format!("{}{}{}", self.char, middle, end)
+        }
+    }
+}
+
+impl Chunk {
+    pub fn new(char: char) -> Self {
+        Self {
+            char,
+            children: Vec::new(),
+            parent: None,
+        }
+    }
+
+    pub fn assigned(char: char, assigned: Imp<Chunk>) -> Self {
+        Self {
+            char,
+            children: Vec::new(),
+            parent: Some(assigned),
+        }
+    }
+
+    pub fn match_end(&self, char: char) -> bool {
+        match self.char {
+            '(' => ')' == char,
+            '{' => '}' == char,
+            '[' => ']' == char,
+            '<' => '>' == char,
+            _ => false,
+        }
+    }
+
+    pub fn corrupt(&self) -> bool {
+        let mut r = !matches!(self.char, '(' | '{' | '[' | '<');
+
+        self.children.iter().for_each(|c| r |= c.corrupt());
+        r
+    }
+
+    pub fn first_corrupt(&self) -> Option<char> {
+        let alright = matches!(self.char, '(' | '{' | '[' | '<');
+
+        if alright {
+            let mut ans = None;
+            for c in &self.children {
+                let a = c.first_corrupt();
+                if a.is_some() {
+                    ans = a;
+                    break;
+                }
+            }
+            ans
+        } else {
+            Some(self.char)
+        }
+    }
+}
+
+pub fn part1() -> i32 {
+    let mut paran = 0;
+    let mut square = 0;
+    let mut curly = 0;
+    let mut arrow = 0;
+
+    load_to_matrix("input/day10.txt").for_each(|mut r| {
+        let fst = r.next().unwrap();
+
+        let parent_chunk = Imp::new(Chunk::new(fst));
+        let mut current = Imp::clone(&parent_chunk);
+        let mut done = false;
+
+        r.for_each(|c| {
+            if !done {
+                if current.match_end(c) {
+                    if let Some(p) = &current.parent {
+                        current = Imp::clone(p);
+                    } else {
+                        done = true;
+                    }
+                } else {
+                    current = Imp::new(Chunk::assigned(c, Imp::clone(&current)));
+                    if let Some(mut p) = current.parent.clone() {
+                        p.children.push(current.clone())
+                    }
+                }
+            }
+        });
+
+        if parent_chunk.corrupt() {
+            if let Some(c) = parent_chunk.first_corrupt() {
+                match c {
+                    ')' => paran += 1,
+                    ']' => square += 1,
+                    '}' => curly += 1,
+                    '>' => arrow += 1,
+                    _ => {}
+                }
+            }
+        }
+    });
+
+    (paran * 3) + (square * 57) + (curly * 1197) + (arrow * 25137)
+}
+
+pub fn part2() -> i128 {
+    let mut v = load_to_matrix("input/day10.txt")
+        .map(|mut r| {
+            let raw_str = r.clone().collect::<String>();
+            let fst = r.next().unwrap();
+
+            let mut parent_chunk = Imp::new(Chunk::new(fst));
+            let mut current = Imp::new(Chunk::assigned(fst, Imp::clone(&parent_chunk)));
+            parent_chunk.children.push(current.clone());
+
+            r.for_each(|c| {
+                if current.match_end(c) {
+                    if let Some(p) = &current.parent {
+                        current = Imp::clone(p);
+                    }
+                } else {
+                    current = Imp::new(Chunk::assigned(c, Imp::clone(&current)));
+                    if let Some(mut p) = current.parent.clone() {
+                        p.children.push(current.clone())
+                    }
+                }
+            });
+
+            if !parent_chunk.corrupt() {
+                let mut s = format!("{}", parent_chunk);
+
+                for c in raw_str.chars() {
+                    if c == s.chars().next().unwrap() {
+                        s.remove(0);
+                    }
+                }
+
+                let mut score = 0;
+                s.chars().into_iter().for_each(|c| match c {
+                    ')' => score = (score * 5) + 1,
+                    ']' => score = (score * 5) + 2,
+                    '}' => score = (score * 5) + 3,
+                    '>' => score = (score * 5) + 4,
+                    _ => {}
+                });
+                score
+            } else {
+                0
+            }
+        })
+        .filter(|i| *i != 0)
+        .collect::<Vec<_>>();
+    v.sort_unstable();
+    v[v.len() / 2]
 }
