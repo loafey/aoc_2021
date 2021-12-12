ea18dbb13ad396dcde6744a5e72695d61939fb44
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2021-12-12 23:22:06 +0100

    Not going so well this day.

--- src/day12/mod.rs
+++ src/day12/mod.rs
@@ -25,18 +25,17 @@ impl Path {
 }
 impl std::fmt::Display for Path {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.helper(1))
+        write!(f, "{}", self.helper("\n"))
     }
 }
 impl Path {
-    fn helper(&self, tab: usize) -> String {
-        let mut t = String::new();
+    fn helper(&self, last: &str) -> String {
+        let last = format!("{} {}", last, self.name());
         match &self {
             Path::Leaf(s) => s.clone(),
-            Path::Node(s, ps) => {
-                ps.iter()
-                    .for_each(|p| t = format!("{}\n{}{}", t, "│ ".repeat(tab), p.helper(tab + 1)));
-                format!("{}{}", s, t)
+            Path::Node(_, ps) => {
+                last
+                //last
             }
         }
     }
@@ -87,20 +86,57 @@ pub fn part1() -> usize {
             }
         });
 
-    caves.iter().for_each(|(k, c)| {
-        println!("{:<6}|\t{:?}", k, c.roads);
-    });
-
-    let start = evolve(Path::Leaf("start".to_string()), vec![], &caves);
+    let start = evolve(Path::Leaf("start".to_string()), vec![], &caves, false);
 
     start.ends()
 }
 
-pub fn part2() -> String {
-    String::new()
+pub fn part2() -> usize {
+    let s = "input/day12example.txt";
+    let mut caves = HashMap::new();
+    caves.insert("start".to_string(), Cave::new(true));
+    caves.insert("end".to_string(), Cave::new(true));
+
+    load_to_rows_and_pattern(s, |c| c == '-').for_each(|r| {
+        r.for_each(|w| match &w[..] {
+            "start" => {}
+            "end" => {}
+            _ => {
+                let uppercase = !w.chars().next().unwrap().is_lowercase();
+                let cave = Cave::new(uppercase);
+                caves.entry(w).or_insert(cave);
+            }
+        });
+    });
+    load_to_rows_and_pattern(s, |c| c == '-')
+        .map(|mut r| {
+            let a = r.next().unwrap();
+            let b = r.next().unwrap();
+            (a, b)
+        })
+        .for_each(|(a, b)| {
+            let mut a_ref = caves.get(&a).unwrap().clone();
+            let mut b_ref = caves.get(&b).unwrap().clone();
+            if b != "start" && a != "end" {
+                a_ref.roads.push(b.clone());
+            }
+            if a != "start" && b != "end" {
+                b_ref.roads.push(a);
+            }
+        });
+
+    let start = evolve(Path::Leaf("start".to_string()), vec![], &caves, true);
+    println!("{}", start);
+
+    start.ends()
 }
 
-fn evolve(p: Path, explored: Vec<String>, caves: &HashMap<String, Imp<Cave>>) -> Path {
+fn evolve(
+    p: Path,
+    explored: Vec<String>,
+    caves: &HashMap<String, Imp<Cave>>,
+    first_explored: bool,
+) -> Path {
     let n = p.name();
     let r = &caves.get(n).unwrap().roads;
     if r.is_empty() {
@@ -111,16 +147,26 @@ fn evolve(p: Path, explored: Vec<String>, caves: &HashMap<String, Imp<Cave>>) ->
             r.iter()
                 .filter_map(|s| {
                     if !explored.contains(s) {
+                        let mut first_explored = first_explored;
                         let c = caves.get(s).unwrap();
                         let mut n = explored.clone();
-                        if !c.big {
+                        let mut r = n.clone();
+                        let mut res = vec![];
+                        if !c.big && !first_explored {
                             n.push(s.clone());
+                            r.push(s.clone());
+                        } else if !c.big {
+                            n.push(s.clone());
+                            first_explored = false;
+                            res.push(evolve(Path::Leaf(s.clone()), r, caves, first_explored));
                         }
-                        Some(evolve(Path::Leaf(s.clone()), n, caves))
+                        res.push(evolve(Path::Leaf(s.clone()), n, caves, first_explored));
+                        Some(res)
                     } else {
                         None
                     }
                 })
+                .flatten()
                 .collect(),
         )
     }
