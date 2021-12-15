2f768b6acd6882bb1e66ab7d504a0a93f7ce20e2
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2021-12-15 11:54:36 +0100

    Magic :-)

--- src/day13/mod.rs
+++ src/day13/mod.rs
@@ -1,6 +1,4 @@
-use core::panic;
-
-use aoc_lib::load;
+use aoc_lib::{load, math::transpose_vec};
 
 #[derive(Debug)]
 enum Dir {
@@ -60,31 +58,45 @@ fn parse_map(data: &str) -> Vec<Vec<char>> {
     board
 }
 
+fn flip(data: &mut Vec<Vec<char>>, v: usize) {
+    let mut p1 = data[0..v].to_vec();
+    let mut p2 = data[v + data.len() % 2..data.len()].to_vec();
+
+    p2.reverse();
+
+    p2.iter().enumerate().for_each(|(y, r)| {
+        r.iter().enumerate().for_each(|(x, _)| {
+            if p2[y][x] == '#' {
+                p1[y][x] = p2[y][x];
+            }
+        })
+    });
+
+    let flippert = data.len() % 2 == 0;
+    if flippert {
+        let l = p1.len() - 1;
+        for x in 0..p1[l].len() {
+            p1[l][x] = '.';
+        }
+    }
+
+    data.clear();
+    data.append(&mut p1);
+}
+
 fn fold(data: &mut Vec<Vec<char>>, dir: &Dir) {
     match dir {
-        Dir::X(v) => {}
-        Dir::Y(v) => {
-            let mut p1 = data[0..*v].to_vec();
-            let mut p2 = data[*v + 1..data.len()].to_vec();
-
-            p2.reverse();
-
-            p2.iter().enumerate().for_each(|(y, r)| {
-                r.iter().enumerate().for_each(|(x, _)| {
-                    if p2[y][x] == '#' {
-                        p1[y][x] = p2[y][x];
-                    }
-                })
-            });
-
-            data.clear();
-            data.append(&mut p1);
+        Dir::X(v) => {
+            transpose_vec(data);
+            flip(data, *v);
+            transpose_vec(data);
         }
+        Dir::Y(v) => flip(data, *v),
         Dir::Gof => panic!("What are we doing here?"),
     }
 }
 
-pub fn part1() -> String {
+pub fn part1() -> usize {
     let i = load("input/day13example.txt");
     let mut splot = i.split("\n\n");
 
@@ -96,7 +108,7 @@ pub fn part1() -> String {
         r.iter().for_each(|c| s.push(*c));
         println!("{}", s);
     });
-    instructions.into_iter().for_each(|i| {
+    /*instructions.into_iter().for_each(|i| {
         fold(&mut data, &i);
 
         println!("- {:<3?} -", i);
@@ -105,9 +117,20 @@ pub fn part1() -> String {
             r.iter().for_each(|c| s.push(*c));
             println!("{}", s);
         });
+    });**/
+    println!("--");
+    fold(&mut data, &instructions[0]);
+    data.iter().for_each(|r| {
+        let mut s = String::new();
+        r.iter().for_each(|c| s.push(*c));
+        println!("{}", s);
     });
 
-    String::new()
+    counter(&mut data)
+}
+
+pub fn counter(data: &mut Vec<Vec<char>>) -> usize {
+    data.iter().flatten().filter(|c| **c == '#').count()
 }
 
 pub fn part2() -> String {
